642453c2781f1deeba862d9bd8d1820e0870249e
Petrisor Lacatus <petrisor.lacatus@gmail.com>
2017-02-06 00:27:35 +0200

    Use es5 typescript definitions only. Make sure definitions are only loaded once

--- ui/monacoScriptEditor/monacoEditor.js
+++ ui/monacoScriptEditor/monacoEditor.js
@@ -24,6 +24,7 @@ TW.jqPlugins.twCodeEditor.prototype.showCodeProperly = function () {
     var thisPlugin = this;
     var jqEl = thisPlugin.jqElement;
     var codeTextareaElem = jqEl.find('.code-container');
+    var vsRoot = '/Thingworx/Common/extensions/MonacoScriptEditor/ui/monacoScriptEditor/vs';
     jqEl.find('.btn-toolbar').hide();
     codeTextareaElem.height("100%");
     codeTextareaElem.css("min-height", "365px");
@@ -47,39 +48,50 @@ TW.jqPlugins.twCodeEditor.prototype.showCodeProperly = function () {
 
     require.config({
         paths: {
-            'vs': '/Thingworx/Common/extensions/MonacoScriptEditor/ui/monacoScriptEditor/vs'
+            'vs': vsRoot
         }
     });
     require(['vs/editor/editor.main'], function () {
         if (thisPlugin.properties) {
+            if (!TW.jqPlugins.twCodeEditor.initializedDefaults) {
+                // compiler options
+                monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
+                    target: monaco.languages.typescript.ScriptTarget.ES5,
+                    allowNonTsExtensions: true,
+                    noLib: true
+                });
+                var coreDefsName = 'lib.es5.d.ts';
+                $.get(vsRoot + "/language/typescript/lib/" + coreDefsName, function (data) {
+                    // Register the es5 library
+                    monaco.languages.typescript.javascriptDefaults.addExtraLib(
+                        data,
+                        coreDefsName
+                    );
+                });
 
-            // compiler options
-            monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
-                target: monaco.languages.typescript.ScriptTarget.ES5,
-                allowNonTsExtensions: true
-            });
-
-            // extra libraries
-            monaco.languages.typescript.javascriptDefaults.addExtraLib([
-                'declare class logger {',
-                '    /**',
-                '     * Log a debug warning',
-                '     */',
-                '    static debug(message:string)',
-                '    /**',
-                '     * Log a error warning',
-                '     */',
-                '    static error(message:string)',
-                '    /**',
-                '     * Log a warn warning',
-                '     */',
-                '    static warn(message:string)',
-                '    /**',
-                '     * Log a info warning',
-                '     */',
-                '    static info(message:string)',
-                '}',
-            ].join('\n'), 'thingworx/logger.d.ts');
+                // extra libraries
+                monaco.languages.typescript.javascriptDefaults.addExtraLib([
+                    'declare class logger {',
+                    '    /**',
+                    '     * Log a debug warning',
+                    '     */',
+                    '    static debug(message:string)',
+                    '    /**',
+                    '     * Log a error warning',
+                    '     */',
+                    '    static error(message:string)',
+                    '    /**',
+                    '     * Log a warn warning',
+                    '     */',
+                    '    static warn(message:string)',
+                    '    /**',
+                    '     * Log a info warning',
+                    '     */',
+                    '    static info(message:string)',
+                    '}',
+                ].join('\n'), 'thingworx/logger.d.ts');
+                TW.jqPlugins.twCodeEditor.initializedDefaults = true;
+            }
             var thingModel = jqEl.closest("tbody").find(".twServiceEditor").twServiceEditor("getAllProperties").model;
             var meClass = "declare class me {";
             var propertyDefs = thingModel.attributes.thingShape.propertyDefinitions;
@@ -88,10 +100,10 @@ TW.jqPlugins.twCodeEditor.prototype.showCodeProperly = function () {
                 if (!propertyDefs.hasOwnProperty(key)) continue;
 
                 var obj = propertyDefs[key];
-                meClass += "/** \n * " + obj.description + " \n */" + "\n static " + obj.name + ":" + obj.baseType +";\n";
+                meClass += "/** \n * " + obj.description + " \n */" + "\n static " + obj.name + ":" + obj.baseType + ";\n";
             }
-            meClass = meClass +  "\n}";
-            
+            meClass = meClass + "\n}";
+
             monaco.languages.typescript.javascriptDefaults.addExtraLib(meClass, 'thingworx/me.d.ts');
             // avalible options: https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.ieditoroptions.html
             var editor = monaco.editor.create(codeTextareaElem[0], {
