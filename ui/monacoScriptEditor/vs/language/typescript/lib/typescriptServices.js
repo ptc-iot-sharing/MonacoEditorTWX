6d1ada413a908d7b9ebc0f5fbdbf2acadabc9f75
Petrisor Lacatus <placatus@ptc.com>
2018-05-21 15:40:53 +0300

    Updated to monaco 0.13.1

--- ui/monacoScriptEditor/vs/language/typescript/lib/typescriptServices.js
+++ ui/monacoScriptEditor/vs/language/typescript/lib/typescriptServices.js
@@ -1,6 +1,6 @@
 /*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * monaco-typescript version: 3.1.0(32694b5dec0171f7d99468c43a9e58f4ea4a2921)
+ * monaco-typescript version: 3.1.0(b24c31d76643a9f351c32043936a22042d8f2409)
  * Released under the MIT license
  * https://github.com/Microsoft/monaco-typescript/blob/master/LICENSE.md
  *-----------------------------------------------------------------------------*/
