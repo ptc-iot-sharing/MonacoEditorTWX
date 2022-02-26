42820cb846ae6764564e0e44ff1bf72be566bd85
Petrisor Lacatus <placatus@iqnox.com>
2022-02-26 15:37:34 +0200

    fix: Drop emmet related code, since it wasn't working even before. Will be added back once upstream is fixed

--- src/editors/cssEditor/cssEditor.ts
+++ src/editors/cssEditor/cssEditor.ts
@@ -1,20 +1,8 @@
 import { MonacoCodeEditor } from "../basicCodeEditor";
-import { emmetCSS } from "emmet-monaco-es";
 
 export class CssEditor extends MonacoCodeEditor {
-    emmetDispose: ()=>void;
 
     constructor(container, initialSettings, actionCallbacks, instanceSettings) {
         super(container, initialSettings, actionCallbacks, instanceSettings);
-        this.emmetDispose = emmetCSS();
     }
-
-    /**
-     * Dispose of the given editor, and emmet
-     */
-    public dispose() {
-        super.dispose();
-        this.emmetDispose();
-    }
-
 }
